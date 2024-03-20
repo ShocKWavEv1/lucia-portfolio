4ad1cbee2999825ab80ff80c4d7f56ac10d430db
AxellGuillen <arianowsky@gmail.com>
2024-03-19 21:07:01 -0600

    prueba axell

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Box w="100%">
       <Box w="100%" h="100vh" bg="primary.200">
-        Hi View
+        <Heading fontSize="80px" color="primary.900">
+          Hola texto
+        </Heading>
+        <Button colorScheme="primary">Button</Button>
       </Box>
     </Box>
   );
